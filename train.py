d2e97960313640fde46f5b30c571fcd5a5977526
Gyunghun Yu <139361822+Alexhyungnim@users.noreply.github.com>
2025-01-02 23:05:45 +0900

    Create train.py

---
+++ train.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import Custom_Modules.modules as md
+import os
+import pandas as pd
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+ALPHA, BETA = 0.1, 1.0  # Hamiltonian, Regularization
+EXJ, DMN, HEXTZ, KZ = 1.0, 0.5, 0.0, 0.5
+EPOCHS = 2000
+BATCH_SIZE = 2
+
+save_dir = f"models/{ALPHA}_{BETA}/{EXJ}_{DMN}_{HEXTZ}_{KZ}/"
+
+x_train = np.load(
+    "Data/ordinary_train/circle.npy")  # "train.npy", "circles_skynum.npy", "circle.npy", "square.npy", "triangle.npy"
+y_train = np.ones((len(x_train), 1), dtype=np.float32)
+x_valid = np.concatenate(
+    [np.load("Data/ordinary_train/{}".format(path)) for path in ["circle.npy", "square.npy", "triangle.npy"]], axis=0)
+y_valid = np.ones((len(x_valid), 1), dtype=np.float32)
+
+#stride가 홀수면 KS가 홀수 stride가 짝수면 KS가 짝수
+os.makedirs(save_dir, exist_ok=True)
+if os.listdir(save_dir) == []:
+    model_dir = save_dir + "0"
+else:
+    model_dir = save_dir + str(max([int(path) for path in os.listdir(save_dir)]) + 1)
+
+model = md.cnn_model(
+    alpha=ALPHA,
+    beta=BETA,
+    exJ=EXJ,
+    DMN=DMN,
+    Hextz=HEXTZ,
+    Kz=KZ,
+)
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss='mse')
+model.nn.summary()
+# callbacks = md.get_callbacks(os.path.join(model_dir, "ckpt", "Ep{epoch:04d}"))
+callbacks = [md.CustomCallback(model_dir, (x_valid, y_valid))]
+history = model.fit(
+    x_train,
+    y_train,
+    epochs=EPOCHS,
+    batch_size=BATCH_SIZE,
+    validation_data=(x_valid, y_valid),
+    callbacks=callbacks
+)
+
+#save losses
+df_history_data = pd.DataFrame(history.history)
+df_history_data.index += 1
+df_history_data.to_csv(os.path.join(model_dir, "history.csv"))
+print("Training Complete")
